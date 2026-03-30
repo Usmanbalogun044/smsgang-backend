bb70648b8aa902d3e42b0029b51a62cbf056615c
usmanbalogun044 <usmanbalogun044@gmail.com>
2026-03-30 22:54:23 +0100

    feat: enhance countriesForService to include additional operator data fields

--- app/Http/Controllers/Api/ServiceController.php
+++ app/Http/Controllers/Api/ServiceController.php
@@ -83,7 +83,7 @@ class ServiceController extends Controller
                             return null;
                         }
 
-                        return [
+                        $operatorData = [
                             'name' => (string) $operatorName,
                             'cost' => $cost,
                             'count' => $count,
@@ -93,6 +93,59 @@ class ServiceController extends Controller
                                 (float) $row->markup_value,
                             ),
                         ];
+
+                        // Include success_rates if available from provider
+                        if (isset($info['success_rates']) && is_array($info['success_rates'])) {
+                            $operatorData['success_rates'] = array_map(
+                                fn ($rate) => (float) $rate,
+                                $info['success_rates']
+                            );
+                        }
+
+                        // Include rating (overall quality score)
+                        if (isset($info['rating'])) {
+                            $operatorData['rating'] = is_numeric($info['rating']) ? (float) $info['rating'] : null;
+                        }
+
+                        // Include activation speed
+                        if (isset($info['activation_speed'])) {
+                            $operatorData['activation_speed'] = is_numeric($info['activation_speed']) ? (int) $info['activation_speed'] : null;
+                        }
+
+                        // Include average response time (in seconds)
+                        if (isset($info['response_time'])) {
+                            $operatorData['response_time'] = is_numeric($info['response_time']) ? (int) $info['response_time'] : null;
+                        }
+
+                        // Include uptime percentage
+                        if (isset($info['uptime'])) {
+                            $operatorData['uptime'] = is_numeric($info['uptime']) ? (float) $info['uptime'] : null;
+                        }
+
+                        // Include customer reviews/feedback count
+                        if (isset($info['reviews'])) {
+                            $operatorData['reviews'] = is_numeric($info['reviews']) ? (int) $info['reviews'] : null;
+                        }
+
+                        // Include support availability
+                        if (isset($info['support_availability'])) {
+                            $operatorData['support_availability'] = (string) $info['support_availability'];
+                        }
+
+                        // Include refund policy
+                        if (isset($info['refund_policy'])) {
+                            $operatorData['refund_policy'] = (string) $info['refund_policy'];
+                        }
+
+                        // Include any additional provider fields that might be useful
+                        $additionalFields = ['avg_delivery_time', 'sms_speed', 'reliability_score', 'last_update', 'region', 'operator_status'];
+                        foreach ($additionalFields as $field) {
+                            if (isset($info[$field])) {
+                                $operatorData[$field] = $info[$field];
+                            }
+                        }
+
+                        return $operatorData;
                     })
                     ->filter()
                     ->sortBy('cost')
